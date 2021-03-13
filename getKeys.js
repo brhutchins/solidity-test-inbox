7abcf4ab5add3fede1978858ee1a0cc8499c440d
Barnaby Hutchins <bhutchins@gmail.com>
2021-03-13 17:55:17 +0100

    keys are now parsed from a json file

--- getKeys.js
+++ getKeys.js
@@ -1,14 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-// It would be better to do this with a json file, but this will do for now
-function loadKey(keyName) {
-    var keyPath = path.resolve(__dirname, 'keys', keyName);
-    return fs.readFileSync(keyPath, 'utf8').trim();
-}
+let keyPath = path.resolve(__dirname, 'keys/keys.json');
+let keys = JSON.parse(fs.readFileSync(keyPath, 'utf8'));
 
-const mnemonic = loadKey('mnemonic');
-const infuraRinkeby = loadKey('infura-rinkeby');
-
-exports.mnemonic = mnemonic;
-exports.infuraRinkeby = infuraRinkeby;
+module.exports = keys;
