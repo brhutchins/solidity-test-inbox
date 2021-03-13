d6f0978c87a266acf4d5fead9b5e18498bedc405
Barnaby Hutchins <bhutchins@gmail.com>
2021-03-13 17:04:34 +0100

    Cleaning up

--- getKeys.js
+++ getKeys.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const fs = require('fs');
 
+// It would be better to do this with a json file, but this will do for now
 function loadKey(keyName) {
     var keyPath = path.resolve(__dirname, 'keys', keyName);
     return fs.readFileSync(keyPath, 'utf8').trim();
-
 }
 
 const mnemonic = loadKey('mnemonic');
